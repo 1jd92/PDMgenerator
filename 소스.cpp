05d5eca968c76a2a7c370d5394e97e9f2575efbe
1jd92 <86125216+1jd92@users.noreply.github.com>
2021-06-26 02:43:07 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -3,43 +3,47 @@
 #include <stdlib.h>
 #include <string.h>
 int main(int argc, char* argv[]) {
+	int samplingrate;
+	FILE *f;
+	if(f=fopen("config.txt", "r")) {
+		fscanf(f, "%d", &samplingrate);
+		fclose(f);
+	}
+	else {
+		f = fopen("config.txt", "w");
+		fprintf(f, "384000");
+		fclose(f);
+		samplingrate = 384000;
+	}
 	for (int i = 1; i < argc; i++) {
 		printf("%s\n", argv[i]);
 		FILE *f;
 		long hdr[11], smpl, depth, count;
 		double  dsmpl, div,  div2;
-		int cnt, outl, lp, j;
+		int cnt, outl, lp, lp2, j;
 		void *data;
 		char *out;
-		short *sdata, smax;
+		short *sdata, smax, sdatmp;
 		f = fopen(argv[i], "rb");
 		fread(hdr, 4, 11, f);
 		smpl = hdr[6];
 		dsmpl = static_cast<double>(smpl);
-		div = 384000. / dsmpl;
+		div = static_cast<double>(samplingrate) / dsmpl;
 		depth = hdr[8] >> 19;
 		count = hdr[10] / depth;
 		data = malloc(hdr[10]);
 		fread(data, depth, count, f);
 		fclose(f);
-		outl = static_cast<long>(static_cast<long long>(count) * 384000ll / static_cast<long long>(smpl));
+		outl = static_cast<long>(static_cast<long long>(count) * static_cast<long long>(samplingrate) / static_cast<long long>(smpl));
 		out = reinterpret_cast<char*>(malloc(outl));
 		cnt = 0;
 		lp = 0;
-		smax = 0; 
 		sdata = reinterpret_cast<short*>(data);
-		for (j = 0; j < count; j++) {
-			if (smax < sdata[j]) smax = sdata[j];
-			else if (smax < -sdata[j]) smax = -sdata[j];
-		}
-		div2 = 22. / static_cast<double>(smax);
 		while (cnt < outl) {
-			if (cnt > 20) {
-				if (out[cnt - 20] != 0) lp--;
-				else lp++;
-			}
-			if (lp < sdata[static_cast<int>(static_cast<double>(cnt) / div)] * div2)out[cnt] = 255, lp++;
-			else out[cnt] = 0, lp--;
+			sdatmp = sdata[static_cast<int>(static_cast<double>(cnt) / div)];
+			lp -= sdatmp;
+			if (0 > lp)out[cnt] = 255, lp+=32767;
+			else out[cnt] = 0, lp-=32768;
 			cnt++;
 		}
 		free(data);
@@ -54,8 +58,8 @@ int main(int argc, char* argv[]) {
 		hdr[3] = 0x20746D66;
 		hdr[4] = 0x00000010;
 		hdr[5] = 0x00010001;
-		hdr[6] = 384000;
-		hdr[7] = 384000;
+		hdr[6] = samplingrate;
+		hdr[7] = samplingrate;
 		hdr[8] = 0x00080001;
 		hdr[9] = 0x61746164;
 		hdr[10] = outl;
